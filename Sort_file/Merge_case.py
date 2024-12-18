aed1614afd24477988f369056472e9d0b81801f2
Fujimoto <god.carnagr6363@gmail.com>
2024-12-18 20:06:50 +0700

    update file

---
+++ Sort_file/Merge_case.py
@@ -0,0 +1,58 @@
+import timeit
+
+start_time = timeit.default_timer()
+
+def Merge_revers(a_list):
+    n = len(a_list)
+    
+    if n < 2:
+        return a_list
+
+    mid = n // 2
+    left = a_list[:mid]
+    right = a_list[mid:]
+
+    Merge_revers(left)
+    Merge_revers(right)
+
+    a = 0  
+    s = 0  
+    d = 0 
+
+    while a < len(left) and s < len(right):
+        if left[a] > right[s]:
+            a_list[d] = left[a]
+            a += 1
+        else:
+            a_list[d] = right[s]
+            s += 1
+        d += 1
+
+    # Ensure correct indexing in the second while loop for left and right
+    while a < len(left):
+        a_list[d] = left[a]
+        a += 1  # Increment a, not s
+        d += 1
+
+    while s < len(right):
+        a_list[d] = right[s]
+        s += 1  # Increment s here
+        d += 1
+
+    return a_list
+
+file = 'tambol'
+list_file = []
+with open(f'Sorting-Algorithm/Convert/{file}.txt', 'r', encoding='utf-8') as files:
+    for line in files:
+        list_file.append(line.strip())
+        
+list_reversed = Merge_revers(list_file)
+
+output_file = f'Sorting-Algorithm/Sort_file/output/Merge/{file}_Merge.txt'
+with open(output_file, 'w', encoding='utf-8') as write_province:
+    for row in list_reversed:
+        write_province.write(f"{row}\n")
+        
+Timespent = timeit.default_timer() - start_time
+print(f"{Timespent} seconds")
---
+++ Sort_file/lnsertion_case.py
@@ -0,0 +1,30 @@
+import timeit
+start_time = timeit.default_timer()
+
+def lnsertion_revers(a_list):
+    n = len(a_list)
+    for i in range(1, n):
+        temp = a_list[i]
+        hole = i
+        while hole > 0 and a_list[hole - 1] < temp:
+            a_list[hole] = a_list[hole - 1]
+            hole -= 1
+        a_list[hole] = temp
+    return a_list
+
+file = 'tambol' #เปลี่ยนชื่อไฟล์ได้
+list_file = []
+with open(f'Sorting-Algorithm/Convert/{file}.txt', 'r', encoding='utf-8') as files:
+    for line in files:
+        list_file.append(line.strip())
+        
+
+list_reversed = lnsertion_revers(list_file)
+
+output_file = f'Sorting-Algorithm/Sort_file/output/lnsertion/{file}_lnsertion_.txt'
+with open(output_file, 'w', encoding='utf-8') as write_province:
+    for row in list_reversed:
+        write_province.write(f"{row}\n")
+        
+Timespent = timeit.default_timer() - start_time
+print(f"{Timespent} seconds")
